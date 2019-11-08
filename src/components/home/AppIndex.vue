20221f2e8490fa6baa125f665374709b6d895a4f
zhangmk <zmk95511@163.com>
2019-11-09 01:59:20 +0800

    commit

---
+++ src/components/home/AppIndex.vue
@@ -0,0 +1,13 @@
+<template>
+  <dev>hello this is my vue</dev>
+</template>
+
+<script>
+export default {
+  name: 'AppIndex'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import Login from '@/components/Login'
+import AppIndex from '@/components/home/AppIndex'
+import HelloWorld from '@/components/HelloWorld'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login,
+      meta: {
+        requireAuth: true
+      }
+    },
+    {
+      path: '/index',
+      name: 'AppIndex',
+      component: AppIndex,
+      meta: {
+        requireAuth: true
+      }
+    }
+  ]
+})
